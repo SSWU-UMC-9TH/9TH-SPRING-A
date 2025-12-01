8fbef8be3c7a54310d8be558fa6bc36175bf27c6
wonee1 <gimchaewon417@gmail.com>
2025-12-01 19:23:48 +0900

    feat : 미션 목록 조회 api 수정

--- src/main/java/spring/umc/domain/mission/dto/MissionResDTO.java
+++ src/main/java/spring/umc/domain/mission/dto/MissionResDTO.java
@@ -1,6 +1,7 @@
 package spring.umc.domain.mission.dto;
 
 import lombok.Builder;
+import java.time.LocalDateTime;
 
 public class MissionResDTO {
 
@@ -10,5 +11,24 @@ public class MissionResDTO {
             Long userId,
             Long missionId,
             String status
+    ) {}
+
+    /** 특정 가게의 미션 목록 조회 DTO */
+    @Builder
+    public record MissionListDTO(
+            Long missionId,
+            String missionName,
+            Integer rewardPoint,
+            String storeName
+    ) {}
+
+    /** 내가 진행중인 미션 목록 조회 DTO */
+    @Builder
+    public record MyMissionDTO(
+            Long userMissionId,
+            String missionName,
+            String storeName,
+            String status,
+            LocalDateTime assignedAt
     ) {}
 }
