8c748eb8b2fb824cb38e093b14adb250ec7ebd6d
sin._.0_ <138271153+sinyoung6491@users.noreply.github.com>
2025-12-14 21:14:13 +0900

    Revert "Revert "Feat/chapter10 bunny""

--- src/main/java/spring/umc/domain/member/service/query/MemberQueryServiceImpl.java
+++ src/main/java/spring/umc/domain/member/service/query/MemberQueryServiceImpl.java
@@ -1,4 +1,48 @@
 package spring.umc.domain.member.service.query;
 
-public class MemberQueryServiceImpl {
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import spring.umc.domain.member.converter.MemberConverter;
+import spring.umc.domain.member.dto.MemberReqDTO;
+import spring.umc.domain.member.dto.MemberResDTO;
+import spring.umc.domain.member.entity.Member;
+import spring.umc.domain.member.exeption.MemberException;
+import spring.umc.domain.member.exeption.code.MemberErrorCode;
+import spring.umc.domain.member.repository.MemberRepository;
+import spring.umc.global.auth.detail.CustomUserDetails;
+import spring.umc.global.jwt.JwtUtil;
+
+@Service
+@RequiredArgsConstructor
+public class MemberQueryServiceImpl implements MemberQueryService{
+
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder encoder;
+
+    @Override
+    public MemberResDTO.LoginDTO login(
+            MemberReqDTO.@Valid LoginDTO dto
+    ) {
+
+        // Member 조회
+        Member member = memberRepository.findByEmail(dto.email())
+                .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND));
+
+        // 비밀번호 검증
+        if (!encoder.matches(dto.password(), member.getPassword())){
+            throw new MemberException(MemberErrorCode.INVALID);
+        }
+
+        // JWT 토큰 발급용 UserDetails
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+
+        // 엑세스 토큰 발급
+        String accessToken = jwtUtil.createAccessToken(userDetails);
+
+        // DTO 조립
+        return MemberConverter.toLoginDTO(member, accessToken);
+    }
 }
---
+++ src/main/java/spring/umc/domain/review/service/ReviewQueryServiceImpl.java
@@ -0,0 +1,11 @@
+package spring.umc.domain.review.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.umc.domain.review.dto.ReviewResDTO;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewQueryServiceImpl{
+
+}
